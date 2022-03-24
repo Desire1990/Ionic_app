4b005d7db4733751c0406fffe4ee2fa22d0a21b0
Desire1990 <desnduwim26@gmail.com>
2022-03-24 13:04:55 +0200

    todos api call with axios integrations

---
+++ src/pages/Todos.vue
@@ -0,0 +1,42 @@
+<template>
+	<main-layout pageTitle='Todos'>
+		<h4>Todos List</h4>
+		<!-- <ion-button expand='full' @click='showTodos()'> Show Todos</ion-button> -->
+		<ion-list v-for='todo in myTodos' :key='todo.id'>
+			<ion-item >{{ todo.title }}</ion-item>
+		</ion-list>
+	</main-layout>
+</template>
+<script>
+	import { IonButton, IonList, IonItem} from "@ionic/vue";
+	import axios from 'axios';
+	export default{
+		components:{
+			IonButton, 
+			IonList,
+			IonItem
+		},
+		data(){
+			return {
+				myTodos:this.$store.state.myTodos
+				// myTodos:[]
+			}
+		},
+		mounted(){
+			this.showTodos()
+		},
+		methods:{
+			showTodos(){
+				axios.get(this.$store.state.url, this.header
+				).then((response) => {
+				  this.myTodos = response.data;
+				  // this.myTodos = response.data;
+				  console.log(response.data)
+				}).catch((error) => {
+				  console.error(error);
+				});
+			}
+		}
+		
+	}
+</script>

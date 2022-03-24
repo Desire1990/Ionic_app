4da36bde687b796f25474962eaa94e6f5a78d444
Desire1990 <desnduwim26@gmail.com>
2022-03-24 16:53:26 +0200

    crud integrations in todo pages

--- src/pages/Todos.vue
+++ src/pages/Todos.vue
@@ -1,42 +1,71 @@
 <template>
 	<main-layout pageTitle='Todos'>
-		<h4>Todos List</h4>
-		<!-- <ion-button expand='full' @click='showTodos()'> Show Todos</ion-button> -->
-		<ion-list v-for='todo in myTodos' :key='todo.id'>
-			<ion-item >{{ todo.title }}</ion-item>
-		</ion-list>
+		<div>
+			<h4>Todos List</h4>
+			<!-- <ion-button expand='full' @click='showTodos()'> Show Todos</ion-button> -->
+			<ion-card v-for='todo in allTodos' :key='todo.id'>
+				<ion-card-content>
+					<ion-card-title>{{ todo.description }}</ion-card-title>
+				</ion-card-content>
+				<ion-card-content>
+					<ion-iem>{{todo.title}}</ion-iem>					
+				</ion-card-content>
+			    <ion-item> 
+		           <ion-buttons slot="end">
+		             <ion-icon @click="edit(user.id)" style='color: blue;' :icon="create" slot="end"></ion-icon>
+		             <ion-icon :icon="trash" style='color: red;' slot="end"></ion-icon>
+		           </ion-buttons>
+		        </ion-item>
+		      </ion-card>
+		</div>
+		<template v-slot:footerdata>
+	      <ion-fab vertical="bottom" horizontal="end">
+	        <ion-fab-button routerLink="/todos/add/">
+	          <ion-icon :icon="add"></ion-icon>
+	        </ion-fab-button>
+	      </ion-fab>
+	    </template>
 	</main-layout>
 </template>
 <script>
-	import { IonButton, IonList, IonItem} from "@ionic/vue";
-	import axios from 'axios';
-	export default{
-		components:{
-			IonButton, 
-			IonList,
-			IonItem
-		},
-		data(){
-			return {
-				myTodos:this.$store.state.myTodos
-				// myTodos:[]
-			}
-		},
-		mounted(){
-			this.showTodos()
-		},
-		methods:{
-			showTodos(){
-				axios.get(this.$store.state.url, this.header
-				).then((response) => {
-				  this.myTodos = response.data;
-				  // this.myTodos = response.data;
-				  console.log(response.data)
-				}).catch((error) => {
-				  console.error(error);
-				});
-			}
-		}
-		
-	}
+import { mapGetters, mapActions } from "vuex";
+import {IonCard, IonCardContent, IonButton,IonList, IonCardTitle,IonItem,IonLabel,IonFab,IonFabButton,IonIcon,IonButtons
+} from "@ionic/vue";
+import { add, create,trash } from "ionicons/icons";
+export default {
+  components: {
+    IonButton,IonCardTitle,IonLabel,IonFab,IonFabButton,IonIcon,IonButtons, IonList, IonItem,IonCard, IonCardContent
+  },
+  computed: {
+    ...mapGetters("myTodos", {
+      allTodos: "allTodos",
+    }),
+  },
+  mounted(){
+  	this.fetchTodos()
+  },
+  methods: {
+    ...mapActions("myTodos", {
+      fetchTodos: "fetchTodos",
+      deletTodoAction: "deleteTodo"
+    }),
+    deleteTodo(id){
+      this.deletTodoAction(id);
+    },
+    async showTodos() {
+      await this.fetchTodos();
+    },
+    edit(id){
+      this.$router.push(`/add-or-update/${id}`);
+    }
+  },
+    data() {
+    return {
+      add,
+      create,
+      trash
+    };
+  },
+  
+};
 </script>

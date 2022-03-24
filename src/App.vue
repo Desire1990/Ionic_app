bd15042be59408ebb421c00405f1f9d82b1861fd
Desire1990 <desnduwim26@gmail.com>
2022-03-24 12:59:35 +0200

    base updated

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <SideMenu></SideMenu>
+    <ion-router-outlet id='mymenu'/>
   </ion-app>
 </template>
 
 <script >
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import SideMenu from "@/components/SideMenu.vue"
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    SideMenu
   }
 });
 </script>

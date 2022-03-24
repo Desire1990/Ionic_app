203ea2a97c875b32567b5716d2befc67bdab32f6
Desire1990 <desnduwim26@gmail.com>
2022-03-24 13:00:22 +0200

    main page registered in main.js

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-buttons @click='openMenu()' slot='start'>
+					<ion-icon :icon='menu' slot='start'></ion-icon>
+				</ion-buttons>
+				<ion-title>{{pageTitle}}</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<slot></slot>
+		</ion-content>
+	</ion-page>
+</template>
+<script>
+	import {IonPage, IonHeader, IonToolbar, menuController, IonTitle, IonContent, IonIcon, IonButtons} from "@ionic/vue";
+	import { menu } from "ionicons/icons"
+	export default{
+		props:['pageTitle'],
+		components:{
+			IonPage,
+			IonHeader,
+			IonTitle,
+			IonToolbar,
+			IonContent,
+			IonButtons,
+			IonIcon
+		},
+		data(){
+			return{
+				menu
+			}
+		},
+		methods:{
+			openMenu(){
+				menuController.open('sidemenu');
+			}
+		}
+	}
+</script>
